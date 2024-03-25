5e92516f7862588ab84d5d4e4fb5efe34dbf1ead
Marko132001 <markokozlov@gmail.com>
2024-03-25 10:24:06 +0100

    Refactoring code from Test to Game class.

--- app/src/main/java/com/example/projectapp/TestClass.kt
+++ app/src/main/java/com/example/projectapp/TestClass.kt
@@ -8,54 +8,6 @@ import com.example.projectapp.model.Player
 import com.example.projectapp.model.User
 
 
-fun gameRoundSim(game: Game, playerIndex: Int, endRoundIndex: Int){
-
-    var raiseFlag: Boolean = false
-    var playerIndex: Int = playerIndex
-
-    do{
-
-        if(game.players[playerIndex].playerState == PlayerState.FOLD) {
-            playerIndex = (playerIndex + 1) % game.players.size
-            continue
-        }
-
-        println("Select: call, raise, check, fold")
-        var playerAction = readLine()!!
-
-        when(playerAction){
-            "call" -> {
-                game.updatePot(game.players[playerIndex].call(game.currentHighBet))
-                game.players[playerIndex].playerState = PlayerState.CALL
-            }
-            "raise" -> {
-                println("Enter raise amount: ")
-                var raiseAmount = readLine()!!
-
-                game.updatePot(game.players[playerIndex]
-                    .raise(game.currentHighBet, raiseAmount.toInt())
-                )
-                game.currentHighBet = game.players[playerIndex].playerBet
-                game.players[playerIndex].playerState = PlayerState.RAISE
-                raiseFlag = true
-            }
-            "check" -> game.players[playerIndex].playerState = PlayerState.CHECK
-            "fold" -> game.players[playerIndex].playerState = PlayerState.FOLD
-        }
-
-        playerIndex = (playerIndex + 1) % game.players.size
-
-        println(game.toString())
-        println(game.players[0].toString())
-        println(game.players[1].toString())
-        println(game.players[2].toString())
-
-    }while((playerIndex != endRoundIndex && !raiseFlag) ||
-        (game.players[playerIndex].playerBet != game.currentHighBet)
-    )
-
-}
-
 fun main() {
     //Create user accounts
     val user1 = User("User1")
@@ -74,12 +26,8 @@ fun main() {
     //Init the game and add players
     val game = Game(listOfPlayers)
 
-    //TODO: "Reset certain player and game parameters before the beginning of each new game."
 
     for(round in GameRound.entries){
-        //TODO: "Move playerIndex and endRoundIndex in class context"
-        var playerIndex: Int
-        var endRoundIndex: Int
         var round: GameRound = round
 
         if(game.players.count{it.playerState == PlayerState.FOLD} == game.players.size - 1){
@@ -88,27 +36,8 @@ fun main() {
 
         when(round) {
             GameRound.PREFLOP -> {
-                game.generateCommunityCards()
-                game.generateHoleCards()
-
-                //Move dealar button position at beginning of each game
-                game.updateDealerButtonPos()
-
-                //Players bet the smallBlind and bigBlind values. Pot is updated.
-                game.updatePot(
-                    game.players[game.getPlayerRolePos(Game.PlayerRoleOffsets.SMALL_BLIND)]
-                        .paySmallBlind(game.smallBlind)
-                )
-                game.updatePot(
-                    game.players[game.getPlayerRolePos(Game.PlayerRoleOffsets.BIG_BLIND)]
-                        .payBigBlind(game.bigBlind)
-                )
-                game.currentHighBet = game.bigBlind
-
-                playerIndex = game.getPlayerRolePos(Game.PlayerRoleOffsets.UNDER_THE_GUN)
-                endRoundIndex = game.getPlayerRolePos(Game.PlayerRoleOffsets.BIG_BLIND)
-
-                gameRoundSim(game, playerIndex, endRoundIndex)
+                game.preflopRoundInit()
+                game.gameRoundSim()
             }
             GameRound.SHOWDOWN -> {
                 println("Community cards: " + game.showStreet(GameRound.SHOWDOWN))
@@ -122,14 +51,8 @@ fun main() {
             }
             else -> {
                 println(game.showStreet(round))
-                playerIndex = game.getPlayerRolePos(Game.PlayerRoleOffsets.SMALL_BLIND)
-
-                while(game.players[playerIndex].playerState == PlayerState.FOLD){
-                    playerIndex = (playerIndex + 1) % game.players.size
-                }
-                endRoundIndex = playerIndex
-
-                gameRoundSim(game, playerIndex, endRoundIndex)
+                game.streetRoundInit()
+                game.gameRoundSim()
             }
         }
     }
--- app/src/main/java/com/example/projectapp/model/Game.kt
+++ app/src/main/java/com/example/projectapp/model/Game.kt
@@ -1,18 +1,22 @@
 package com.example.projectapp.model
 
 import com.example.projectapp.data.GameRound
+import com.example.projectapp.data.PlayerState
 import com.example.projectapp.data.PlayingCard
 import com.example.projectapp.data.Rank
 import com.example.projectapp.data.Suit
 import kotlin.random.Random
 
 class Game(var players: MutableList<Player>): TableActions {
+
     var potAmount: Int = 0
     var currentHighBet: Int = 0
     val smallBlind: Int = 25
     val bigBlind: Int = 50
     private var communityCards: MutableList<PlayingCard> = mutableListOf()
     var dealerButtonPos: Int = -1
+    var currentPlayerIndex: Int = -1
+    var endRoundIndex: Int = -1
 
     object CardConstants {
         const val COMMUNITY_CARDS = 5
@@ -27,6 +31,48 @@ class Game(var players: MutableList<Player>): TableActions {
         const val SMALL_BLIND = 1
         const val BIG_BLIND = 2
         const val UNDER_THE_GUN = 3
+    }
+
+    fun preflopRoundInit() {
+
+        for(player in players){
+            player.playerState = PlayerState.NONE
+            player.playerBet = 0
+        }
+
+        updateDealerButtonPos()
+
+        generateHoleCards()
+        generateCommunityCards()
+
+        currentPlayerIndex = getPlayerRolePos(PlayerRoleOffsets.UNDER_THE_GUN)
+        endRoundIndex = getPlayerRolePos(PlayerRoleOffsets.BIG_BLIND)
+        potAmount = 0
+        currentHighBet = 0
+
+        updatePot(
+            players[getPlayerRolePos(PlayerRoleOffsets.SMALL_BLIND)]
+                .paySmallBlind(smallBlind)
+        )
+        updatePot(
+            players[getPlayerRolePos(PlayerRoleOffsets.BIG_BLIND)]
+                .payBigBlind(bigBlind)
+        )
+        currentHighBet = bigBlind
+
+    }
+
+    fun streetRoundInit(){
+        currentPlayerIndex = getPlayerRolePos(PlayerRoleOffsets.SMALL_BLIND)
+
+        while(players[currentPlayerIndex].playerState == PlayerState.FOLD){
+            iterateCurrentPlayerIndex()
+        }
+        endRoundIndex = currentPlayerIndex
+    }
+
+    fun iterateCurrentPlayerIndex(){
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.size
     }
 
     override fun updatePlayerList() {
@@ -46,7 +92,9 @@ class Game(var players: MutableList<Player>): TableActions {
             .shuffled().take(CardConstants.HOLE_CARDS * players.size)
 
         for((index, player) in players.withIndex()){
-            player.assignHoleCards(Pair(generatedHoleCards[index*2], generatedHoleCards[index*2 + 1]))
+            player.assignHoleCards(
+                Pair(generatedHoleCards[index*2], generatedHoleCards[index*2 + 1])
+            )
         }
     }
 
@@ -80,5 +128,50 @@ class Game(var players: MutableList<Player>): TableActions {
 
     override fun toString(): String {
         return "Game(potAmount=$potAmount, currentHighBet=$currentHighBet)"
+    }
+
+    fun gameRoundSim() {
+        var raiseFlag = false
+
+        do{
+            if(players[currentPlayerIndex].playerState == PlayerState.FOLD) {
+                iterateCurrentPlayerIndex()
+                continue
+            }
+
+            println("Select: call, raise, check, fold")
+            var playerAction = readLine()!!
+
+            when(playerAction){
+                "call" -> {
+                    updatePot(players[currentPlayerIndex].call(currentHighBet))
+                    players[currentPlayerIndex].playerState = PlayerState.CALL
+                }
+                "raise" -> {
+                    println("Enter raise amount: ")
+                    var raiseAmount = readLine()!!
+
+                    updatePot(players[currentPlayerIndex]
+                        .raise(currentHighBet, raiseAmount.toInt())
+                    )
+                    currentHighBet = players[currentPlayerIndex].playerBet
+                    players[currentPlayerIndex].playerState = PlayerState.RAISE
+                    raiseFlag = true
+                }
+                "check" -> players[currentPlayerIndex].playerState = PlayerState.CHECK
+                "fold" -> players[currentPlayerIndex].playerState = PlayerState.FOLD
+            }
+
+            iterateCurrentPlayerIndex()
+
+            println(toString())
+            println(players[0].toString())
+            println(players[1].toString())
+            println(players[2].toString())
+
+        }while((currentPlayerIndex != endRoundIndex && !raiseFlag) ||
+            (players[currentPlayerIndex].playerBet != currentHighBet)
+        )
+
     }
 }
